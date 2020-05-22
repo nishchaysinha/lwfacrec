adba3104f1a091c06caa4f1fa3b661ebe41b06cd
Nishchay Sinha <61420072+nishchaysinha@users.noreply.github.com>
2020-05-22 18:19:06 +0530

    prolly needs some optimizations
    heheheheheheheeh finnaly cv2 wasnt being stupid

--- lwfac_recog/facial_recog.py
+++ lwfac_recog/facial_recog.py
@@ -1,25 +1,41 @@
+'''
+GIT-"nishchaysinha.github.io"
+
+to install type "git clone 'repo link'"
+
+'''
+
 import cv2
 import sys
 
-face_cascade=(cv2.CascadeClassifier('opencv/data/haarcascades/haarcascade_frontalface_default.xml').load)
-video_capture=cv2.VideoCapture(0)
-test=cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+video_capture = cv2.VideoCapture(0)
 
 while True:
-    #frame by frame analysis
-    retval, frame=video_capture.read()
-    gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces=test.detectMultiScale(
+    #frame-by-frame(24img/sec(default))
+    ret, frame = video_capture.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
         gray,
         scaleFactor=1.1,
         minNeighbors=5,
-        minSize=(35,35)
-)
+        minSize=(30, 30),
+        flags=cv2.cv2.CASCADE_SCALE_IMAGE
+    )
 
-for(x,y,w,h) in faces:
-    cv2.rectangle(frame, (x,y), (x+w, y+h), (50, 50, 200), 2)
-cv2.imshow('Video', frame)
+    #rectangle-overlay
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-if cv2.waitKey(1) & 0xFF==ord('q'):
-    sys.exit()
-    
+    #display-with-rectangle-overlay
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+#"CLEAR CACHE"
+video_capture.release()
+cv2.destroyAllWindows()
